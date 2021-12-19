5757aa931cd7b94e76463d17b12d6064f73b186a
carlosarturoleon <ovalleac@uninorte.edu.co>
2021-12-19 12:06:22 -0500

    Arreglando las mayusculas

--- src/components/NewTournamentComponent.js
+++ src/components/NewTournamentComponent.js
@@ -7,98 +7,93 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 // const minLength = (len) => (val) => (val) && (val.length >= len);
 // const isNumber = (val) => !isNaN(Number(val));
 
-const NewTournament = ({bus}) => {
+class NewTournament extends Component {
 
-    const sending_form = (e) => {
-        e.preventDefault()
-    
-        const tournament = {
-          tournamentname : e.target.tournamentname.value,
-          place : e.target.place.value,
-          date : e.target.date.value,
-          numberofteams : e.target.numberofteams.value,
-          prize : e.target.prize.value,
-          state : e.target.state.value
+    constructor(props) {
+        super(props);
 
-        }
-    
-        bus(tournament)
-    
-      }
-    
-    return(
-        <div className="container">
-            <div className="row row-content">
-                <div className="col-12">
-                    <h1>Tournament Information</h1>
-                    <LocalForm onSubmit={sending_form}>
-                        <Row className="form-group">
-                            <Label htmlFor="tournamentname" md={2}>Tournament Name</Label>
-                            <Col md={10}>
-                                <Control.text model=".tournamentname" id="tournamentname" name="tournamentname" placeholder="Tournament Name" className="form-control" 
-                                // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
-                                />
-                                {/* <Errors className="text-danger" model=".tournamentname" show="touched" message={{required: 'Required', minLength: 'Must be greater than 2 charaters', maxLength: 'Must be 15 characters or less'}}/> */}
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Label htmlFor="place" md={2}>Place</Label>
-                            <Col md={10}>
-                                <Control.text model=".place" id="place" name="place" placeholder="Place" className="form-control" 
-                                // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
-                                />
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Label htmlFor="date" md={2}>Date</Label>
-                            <Col md={10}>
-                                <Control.text model=".date" id="date" name="date" placeholder="Date" className="form-control"/>
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Label htmlFor="numberofteams" md={2}>Number of Teams</Label>
-                            <Col md={10}>
-                                <Control.text model=".numberofteams" id="numberofteams" name="numberofteams" placeholder="Number of Teams" className="form-control" 
-                                // validators={{ required, isNumber }}
-                                />
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Label htmlFor="prize" md={2}>Prize</Label>
-                            <Col md={10}>
-                                <Control.text model=".prize" id="prize" name="prize" placeholder="Prize" className="form-control" 
-                                // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
-                                />
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Label htmlFor="State" md={2}>State</Label>
-                            <Col md={10}>
-                                <Control.select model=".state" id="state" name="state" placeholder="state" className="dropdown-menu-dark" 
-                                // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
-                                >
-                                    <option>Open Enrollment</option>
-                                    <option>Registrations Closed</option>
-                                    <option>In Progress</option>
-                                    <option>Finished</option>
-                                    <option>Cancelled</option>
-                                </Control.select>
-                            </Col>
-                        </Row>
-                        <Row className="form-group">
-                            <Col md={{size:10, offset:2}}>
-                                <Button  type="submit" id="submitform" color="danger">
-                                    Save Tournament
-                                </Button>
-                            </Col>
-                        </Row>
-                    </LocalForm>
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(values) {
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <div className="row row-content">
+                    <div className="col-12">
+                        <h1>Tournament Information</h1>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="tournamentname" md={2}>Tournament Name</Label>
+                                <Col md={10}>
+                                    <Control.text model=".tournamentname" id="tournamentname" name="tournamentname" placeholder="Tournament Name" className="form-control" 
+                                    // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
+                                    />
+                                    {/* <Errors className="text-danger" model=".tournamentname" show="touched" message={{required: 'Required', minLength: 'Must be greater than 2 charaters', maxLength: 'Must be 15 characters or less'}}/> */}
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="place" md={2}>Place</Label>
+                                <Col md={10}>
+                                    <Control.text model=".place" id="place" name="place" placeholder="Place" className="form-control" 
+                                    // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="date" md={2}>Date</Label>
+                                <Col md={10}>
+                                    <Control.text model=".date" id="date" name="date" placeholder="Date" className="form-control"/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="numberofteams" md={2}>Number of Teams</Label>
+                                <Col md={10}>
+                                    <Control.text model=".numberofteams" id="numberofteams" name="numberofteams" placeholder="Number of Teams" className="form-control" 
+                                    // validators={{ required, isNumber }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="prize" md={2}>Prize</Label>
+                                <Col md={10}>
+                                    <Control.text model=".prize" id="prize" name="prize" placeholder="Prize" className="form-control" 
+                                    // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="State" md={2}>State</Label>
+                                <Col md={10}>
+                                    <Control.select model=".state" id="state" name="State" placeholder="State" className="dropdown-menu-dark" 
+                                    // validators={{ required, minLength: minLength(3), maxLength: maxLength(15)}}
+                                    >
+                                        <option>Open Enrollment</option>
+                                        <option>Registrations Closed</option>
+                                        <option>In Progress</option>
+                                        <option>Finished</option>
+                                        <option>Cancelled</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10, offset:2}}>
+                                    <Button type="submit" id="submitform" color="danger">
+                                        Save Tournament
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </div>
+
                 </div>
-
             </div>
-        </div>
-    )
-    
+        )
+    }
 
 }
 
